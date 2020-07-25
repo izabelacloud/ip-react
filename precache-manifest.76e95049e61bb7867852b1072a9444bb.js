7ddd4bccb863a3186fdf8ca16ca73379743141f5
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-07-25 16:11:38 -0700

    Updates

---
+++ precache-manifest.76e95049e61bb7867852b1072a9444bb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2ed0c501699ef0858ff1c5e1c30ddc",
+    "url": "/ip-react/index.html"
+  },
+  {
+    "revision": "5208d5dd3cf259b007dc",
+    "url": "/ip-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4143c2b1eb775890c15c",
+    "url": "/ip-react/static/css/main.c318d926.chunk.css"
+  },
+  {
+    "revision": "5208d5dd3cf259b007dc",
+    "url": "/ip-react/static/js/2.d581e526.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ip-react/static/js/2.d581e526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4143c2b1eb775890c15c",
+    "url": "/ip-react/static/js/main.1e639581.chunk.js"
+  },
+  {
+    "revision": "ded539353566189d0262",
+    "url": "/ip-react/static/js/runtime-main.c39eee13.js"
+  },
+  {
+    "revision": "354d28e9ff676c1e6b0d83a1b8c94e52",
+    "url": "/ip-react/static/media/cover-image.354d28e9.jpg"
+  },
+  {
+    "revision": "7c356c52b09febac972561319157fde1",
+    "url": "/ip-react/static/media/github2.7c356c52.png"
+  },
+  {
+    "revision": "19b25fbadc22b40b280e4107eb61e882",
+    "url": "/ip-react/static/media/ip2.19b25fba.jpg"
+  }
+]);
